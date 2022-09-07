86ade56c4dcbaec0ad1bc5dabf42e462e55ecb30
harialbie <harialbie@gmail.com>
2022-09-07 20:28:40 +0530

    initial commit

---
+++ seleexamples/src/seleexamples/Selefir.java
@@ -0,0 +1,20 @@
+package seleexamples;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Selefir {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		  System.setProperty("webdriver.chrome.driver",
+		  "E:\\Jarfiles\\chromedriver_win32\\chromedriver.exe");
+		  WebDriver driver=new
+		  ChromeDriver(); driver.get("http://www.google.com");
+		  driver.manage().window().maximize(); driver.quit();
+		 
+	}
+
+}
