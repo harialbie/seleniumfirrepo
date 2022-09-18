92c137e66214c863ea09fad9c4c01fa9cf4a3da9
harialbie <harialbie@gmail.com>
2022-09-18 21:50:34 +0530

    Pushing Extent Reports

--- seleexamples/src/seleexamples/seleniumsec.java
+++ seleexamples/src/seleexamples/seleniumsec.java
@@ -1,7 +1,13 @@
 package seleexamples;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.io.FileHandler;
 import org.testng.Assert;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
@@ -17,18 +23,19 @@ import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 public class seleniumsec {
 	 WebDriver driver;
 	 ExtentReports extentReport;
-	 ExtentSparkReporter html;
+	 ExtentSparkReporter spark;
 	 ExtentTest Testcase;
 	@BeforeSuite
 	public void bs() {
 	
-		html=new ExtentSparkReporter("Extentreport.html");
+		spark=new ExtentSparkReporter("Extentreport.html");
 		extentReport=new ExtentReports();
-		extentReport.attachReporter(html);
+		extentReport.attachReporter(spark);
 		 System.setProperty("webdriver.chrome.driver",
 				  "E:\\Jarfiles\\chromedriver_win32\\chromedriver.exe");
 				   driver=new
 				  ChromeDriver();
+				 //  spark.config().
 	}
 	
 	@AfterSuite
@@ -45,13 +52,27 @@ public class seleniumsec {
     	Testcase.log(Status.PASS, "Opened Google Successfully");
     }
 	@Test
-	public void method2() {
+	public void method2() throws IOException, InterruptedException {
 		/*
 		 * System.out.println("Sample Test Test case 1"); Assert.assertEquals(false,
 		 * true);
 		 */
 		Testcase=extentReport.createTest("Opening Bing");
     	driver.get("https://www.bing.com/");
-    	Testcase.log(Status.PASS, "Opened Bing Successfully");
+    	//Thread.sleep(3000);
+    	if(driver.getTitle().equals("google1")) 
+    	{
+    		Testcase.pass("Values are matching");
+    		Testcase.log(Status.PASS, "Opened Bing Successfully");
+    	}else {
+    		Testcase.fail("Values are not matching");
+    		TakesScreenshot screenshot=(TakesScreenshot)driver;
+    		File source=screenshot.getScreenshotAs(OutputType.FILE);
+    		File destination=new File("extentsample.png");
+    		FileHandler.copy(source, destination);
+    		Testcase.addScreenCaptureFromPath("extentsample.png");
+    		Testcase.log(Status.FAIL, "Not Opened Bing Successfully");
+    	}
+    	
     }
 }
